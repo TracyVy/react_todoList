c435e23768832a359a56cde7587bb6262d11b69f
Tracy Vy <tracyvy88@gmail.com>
2020-08-15 13:04:19 -0700

    Updates

---
+++ precache-manifest.70805763940ba034ec154a904629fb04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef40a561a88456307a067efd8c879de6",
+    "url": "/react_todoList/index.html"
+  },
+  {
+    "revision": "c8d21a04e8dd14d678bc",
+    "url": "/react_todoList/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "fba10e0ed70ef8d7c6af",
+    "url": "/react_todoList/static/js/2.e532ebd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todoList/static/js/2.e532ebd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d21a04e8dd14d678bc",
+    "url": "/react_todoList/static/js/main.3c64cb26.chunk.js"
+  },
+  {
+    "revision": "e4a48c97e84f41bff828",
+    "url": "/react_todoList/static/js/runtime-main.b842b86d.js"
+  }
+]);
